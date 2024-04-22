4b60c71b62cacf715665d2efeea281de38cd1002
Tijs-B <tijs.bergmans@mobilevikings.be>
2024-04-22 23:02:23 +0200

    Admin stuff

--- backend/linker/config/admin.py
+++ backend/linker/config/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 from .models import Setting, Switch
 
-admin.site.register(Setting, admin.ModelAdmin)
-admin.site.register(Switch, admin.ModelAdmin)
+
+@admin.register(Switch)
+class SwitchAdmin(admin.ModelAdmin):
+    list_display = ('name', 'active')
+
+
+@admin.register(Setting)
+class SettingAdmin(admin.ModelAdmin):
+    list_display_ = ('key', 'value', 'description')
