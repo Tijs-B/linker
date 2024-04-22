781f7ad22b41f82826b5cf1e575a46587c1176e3
Tijs-B <tijs.bergmans@mobilevikings.be>
2024-04-22 23:00:13 +0200

    Put tracing teams behind a switch

--- backend/linker/tracing/constants.py
+++ backend/linker/tracing/constants.py
@@ -4,6 +4,8 @@ FICHE_MAX_DISTANCE = 100
 TOCHT_MAX_DISTANCE = 60
 WEIDE_MAX_DISTANCE = 100
 
+SWITCH_TRACE_TEAMS = 'trace_teams'
+
 
 class CheckpointLogType(Enum):
     AUTOMATED = 'automated'
--- backend/linker/tracing/tasks.py
+++ backend/linker/tracing/tasks.py
@@ -2,7 +2,9 @@ from logging import getLogger
 
 from celery import shared_task
 
+from linker.config.models import Switch
 from linker.people.models import Team
+from linker.tracing.constants import SWITCH_TRACE_TEAMS
 from linker.tracing.utils import trace_team
 
 
@@ -11,6 +13,7 @@ logger = getLogger(__name__)
 
 @shared_task
 def trace_teams():
-    logger.info('Starting tracing teams')
-    for team in Team.objects.all():
-        trace_team(team)
+    if Switch.switch_is_active(SWITCH_TRACE_TEAMS):
+        logger.info('Starting tracing teams')
+        for team in Team.objects.all():
+            trace_team(team)
