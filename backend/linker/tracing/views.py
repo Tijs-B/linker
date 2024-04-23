cfe7d8f4f03209f26da2c1a80b0f39938594555b
Tijs-B <tijs.bergmans@mobilevikings.be>
2024-04-23 15:48:55 +0200

    Cache tracing stats

--- backend/linker/tracing/views.py
+++ backend/linker/tracing/views.py
@@ -1,10 +1,16 @@
+from logging import getLogger
+
 from django.contrib.auth.decorators import login_required
+from django.core.cache import cache
 from django.http import JsonResponse
 from rest_framework import viewsets
 
 from linker.tracing.models import CheckpointLog
 from linker.tracing.serializers import CheckpointLogSerializer
 from linker.tracing.stats import calculate_stats
+
+
+logger = getLogger(__name__)
 
 
 class CheckpointLogViewSet(viewsets.ReadOnlyModelViewSet):
@@ -14,5 +20,11 @@ class CheckpointLogViewSet(viewsets.ReadOnlyModelViewSet):
 
 @login_required
 def all_stats(request):
-    stats = calculate_stats()
+    stats = cache.get('tracing_stats')
+    if stats is None:
+        stats = calculate_stats()
+        logger.info('Tracing stats cache miss, regenerated stats')
+        cache.set('tracing_stats', stats, 30)
+    else:
+        logger.info('Tracing stats cache hit')
     return JsonResponse(stats)
