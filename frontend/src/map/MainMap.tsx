c2601d7de9f30b7d91e70de99d8d71ca1912bd87
Tijs-B <tijs.bergmans@mobilevikings.be>
2024-04-23 21:06:54 +0200

    Better geolocate

--- frontend/src/map/MainMap.tsx
+++ frontend/src/map/MainMap.tsx
@@ -234,7 +234,7 @@ const MainMap = memo(function MainMap({ filteredTeams, filteredMembers }: MainMa
         <TrackerHistoryLayer visible={!showHeatmap} />
 
         <NavigationControl />
-        <GeolocateControl />
+        <GeolocateControl trackUserLocation positionOptions={{enableHighAccuracy: true}} />
         <ScaleControl position="bottom-right" />
 
         <CustomOverlay>
