4274b84013afbe62aef46650fb8fab27c23eedcf
Tijs-B <tijs.bergmans@mobilevikings.be>
2024-04-23 22:48:40 +0200

    Add django-extensions

--- backend/linker/settings.py
+++ backend/linker/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'rest_framework',
     'rest_framework_gis',
+    'django_extensions',
     'linker.map',
     'linker.people',
     'linker.tracing',
